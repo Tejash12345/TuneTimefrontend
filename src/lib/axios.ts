923d0b66f3f10d585567ceb7b18f6025291483a8
Tejash12345 <143201585+Tejash12345@users.noreply.github.com>
2025-01-22 20:16:25 +0530

    Update axios.ts

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-	baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api",
+	baseURL: import.meta.env.MODE === "production" ? "https://tunetimebackend-2.onrender.com/api" : "/api",
 });
