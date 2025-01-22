613d27af241da9c50987db2b1d19296a79e30d38
Tejash12345 <143201585+Tejash12345@users.noreply.github.com>
2025-01-22 20:13:59 +0530

    Update useChatStore.ts

--- src/stores/useChatStore.ts
+++ src/stores/useChatStore.ts
@@ -22,7 +22,7 @@ interface ChatStore {
 	setSelectedUser: (user: User | null) => void;
 }
 
-const baseURL = import.meta.env.MODE === "development" ? "http://localhost:5000" : "/";
+const baseURL = import.meta.env.MODE === "production" ? "https://tunetimebackend-2.onrender.com" : "/";
 
 const socket = io(baseURL, {
 	autoConnect: false, // only connect if user is authenticated
